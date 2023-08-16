ff62113ab6441b7f2ddc06c01917b9cc42e0db24
ridegeart <73794853+ridegeart@users.noreply.github.com>
2023-08-16 14:24:33 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -16,11 +16,11 @@ The github form [Github JayPatwardhan]
     - projectroot：train.py 所在路徑。
     - [資料夾說明]
         1. dataPickle_Transform/preimages/train , dataPickle_Transform/preimage/test 的照片移到 dataset/images。
-        2. dataPickle_Transform/picklefiles/meta,train,test 移到 dataset/pickle_files。
+        2. dataPickle_Transform/picklefiles/meta,train,test 移到dataset/pickle_files。
 2. process_dataset.py : 建立只有 FMA Defect 類別的 train.csv /  test.csv。
     - train.csv /  test.csv為 train:test = 9:1。
 3. resize.py : 照片 resize 。
-    - Traintype = True
+    - image_root：更改為照片所在路徑。
     - image_size = 224 #resnet50/Transformer
     - image_size = 160 #resnet101
     - image_size = 299 #inceptionV4
@@ -30,7 +30,6 @@ The github form [Github JayPatwardhan]
         2. i_downsample->downsample
         3. self.relu = nn.ReLU() -> self.relu = nn.ReLU(inplace=True)
         4. max_pool->maxpool
-    - ./ResNet.py 為無加入 CBAM 之模型檔
 5. load_dataset.py:
     - self.meta_filename 更改路徑
 6. train.py：加入自定義訓練集。
@@ -40,25 +39,14 @@ The github form [Github JayPatwardhan]
         - batch_size：62 #resnet50/inceptionV4/resnet101
         - batch_size：42 #Transformer
     - num_workers：BatchSize 參考DataLoader 的 batch_size。
-    - 新增 calculate_accuracy 函數，計算模型準確率。
     - Normal Trained：使用 net 新增想用的訓練模型。
         - ResNet50/ResNet101(num_classes)：輸入預計輸出的類別數。
+        - ResNet50(num_classes,use_cbam=False)：無CBAM的resnet50。
         - VisionTransformer()：呼叫參數參考'Vit_b_16'。
-    - Pretrained Weight：原本
-        - 將權重的輸出改為自定義數據集的num_epoch (以Transformer為例)  
-            checkpoint['heads.head.weight'] = torch.rand((14, 768))  
-            checkpoint['heads.head.bias'] = torch.rand(14)  
-        - 將pretrained_dict與 model_state 配對  
-            pretrained_dict =  {k: v for k, v in checkpoint.items() if k in model_state}  
-        - 更新現有的model_dict  
-            model_state.update(pretrained_dict)  
-        - load更新後的model_state  
-            net.load_state_dict(model_state)  
-    - pg：只訓練可以訓練的參數。
-    - 加入 net.train()，不加的話訓練準確率不會上升。
-    - 沿用 scheduler.step() 更新學習率。
-    - 儲存 training loss 與 acc。
+    - Pretrained Weight：
+        - 先建造模型，更改模型dict後，再load model to device。
     - 加入 checkpoint 機制，儲存測試準確率與訓練準確率最高的模型。
+    - 加入 net.train()，不加的話訓練準確率不會上升。
     - 加入 net.eval() 固定 batchnorm 的參數，不隨 test 數據的輸入對參數做平均 ( batchnorm )。
     - 加入 plot loss 與 acc 部分，並設定在訓練完成後繪圖；pd.DataFrame 的 輸入型態改為 list。
     - lineplot 中的 reset_index(inplace=False) -> reset_index()。
@@ -67,16 +55,16 @@ The github form [Github JayPatwardhan]
 1. dataPickle_detect.py : 資料丟入之前先轉換成detect(pickle)，照片名稱預先處理。
     - save detect pickle 轉換在：dataPickle_Transform/pickle_files/detect
     - save detect image 轉換在： dataPickle_Transform/preimages/detect;
-2. process_detect.py:需先將必要的資料父至於對應資料夾中，會產生出一個detect.csv。
+2. process_detect.py:需先將必要的資料複製於對應資料夾中，會產生出一個detect.csv。
     - detect pickle 複製:(dataPickle_Transform/pickle_files/) 複製貼到 (data/pickle_files/)。
     - image 複製:(dataPickle_Transform/preimages/) 複製貼到 (data/detect_imgs/)。
-    - detect.csv : 程式產生儲存在 dataset/。
 3. resize.py : data/detect_imgs 照片記得 resize。
     - Traintype = False
     - image_size = 224 #resnet50/Transformer
     - image_size = 160 #resnet101
     - image_size = 299 #inceptionV4
-4. detect.py :  Inference預測，結果dataset/result/detect_predict.csv。
+4. detect.py :  Inference預測，結果./dataset/result/detect_predict.csv。
+    - modelName更改為訓練好的權重路徑。
     - 加載模型報錯：RuntimeError:Error(s) in loading state_dict for DataParallel，訓練模型與測試模型環境不一致，加入 load_state_dict(torch.load(modelName)) -> load_state_dict(torch.load(modelName),False)
 
 ## .py檔案稍微修改
@@ -84,19 +72,23 @@ The github form [Github JayPatwardhan]
 1. load_dataset.py : 將輸出改為只有一個 label_1 ，並從 third_labels 取index 而來。
 2. helper.py: 使用 read_meta 函式。
 
-## HeatMap熱力圖
+## HeatMap熱力圖 (3layer_Resnet50)
 - 使用 cam.py 畫圖
-1. current：修改為自己的專案位置
-2. modelName：已訓練好的權重檔
+1. modelName：更改為已訓練好的權重檔的路徑。
 - 單張熱力圖
-1. img_path：欲輸入模型的圖片
-2. CAM_RESULT_PATH：畫好的熱力圖的儲存位置
+1. img_path：欲輸入模型的圖片路徑。
+2. CAM_RESULT_PATH：畫好的熱力圖的儲存路徑。
 - 輸出多張熱力圖
-1. imgpath：欲輸入模型的資料夾位置
-2. CAM_RESULT_PATH：畫好的熱力圖的資料夾位置
+1. imgpath：欲輸入模型的資料夾路徑。
+2. CAM_RESULT_PATH：畫好的熱力圖的儲存資料夾路徑。
+3. CAM_FALSE_PATH：分類錯誤的熱力圖與原圖儲存路徑
+4. CAM_RIGHT_PATH：分類正確的熱力圖與原圖儲存路徑
 - 熱力圖與原圖組合圖
-3. bg：畫布大小，(原圖height*2,原圖width)，(2,1)排列
+3. bg：畫布大小，(原圖width*2,原圖height)，(2,1)排列
 4. bg.save：儲存融合原圖與熱力圖的圖片位置
+- 如果要顯示True class的熱力圖
+1. (line156)取消註解，並註解掉(line157)。
+2. (line161)取消註解，並註解掉(line162)。
 
 # Cifar 100
 
--- cam.py
+++ cam.py
@@ -10,15 +10,15 @@ from model import resnet50
 import torch.nn as nn
 import matplotlib.pyplot as plt
 
-# 获取模型输出的feature/score
 class_num = 14
 
-modelName = 'FMA_3layer_50.pth'
+modelName = 'FMA_3layer_50.pth'  #更換為訓練好的模型
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')
-''' Mode read'''
 model = resnet50.ResNet50()
 model_ft = model.to(device)
-model_ft.load_state_dict(torch.load('FMA_3layer_50.pth', map_location=lambda  storage, loc:storage))  
+model_ft.load_state_dict(torch.load(modelName, map_location=lambda  storage, loc:storage))  
+#讀取最後一層的輸出特徵圖
 model_features = nn.Sequential(*list(model_ft.children())[:-7])
 
 class_ = {0:'CF REPAIR FAIL',1:'PI SPOT-WITH PAR',2:'POLYMER',3:'GLASS BROKEN',4:'PV-HOLE-T',5:'CF DEFECT',6:'CF PS DEFORMATION',7:'FIBER',8:'AS-RESIDUE-E',9:'LIGHT METAL',10:'GLASS CULLET',11:'ITO-RESIDUE-T',12:'M1-ABNORMAL',13:'ESD'}
@@ -30,7 +30,8 @@ model_features.eval()
 for name, para in model_ft.named_parameters():
     print('{}: {}'.format(name, para.shape))
 """
-fc_weights = model_ft.linear_lvl3.weight   #numpy数组取维度fc_weights[0].shape->(5,960)
+#獲得fc層的權重
+fc_weights = model_ft.linear_lvl3.weight   #(14,2048)
 
 data_transform = {
         "train": transforms.Compose([transforms.RandomAffine(40, scale=(.85, 1.15), shear=0),
@@ -40,118 +41,141 @@ data_transform = {
                                     transforms.ToTensor()]),
         "val": transforms.Compose([transforms.ToTensor()])}
 """ 
+#---------------單張熱力圖------------------
+
+img_path = './dataset/images/CF PS DEFORMATION@NP@CF@CF_20220619_B5564PE-1-2.jpg'     ＃圖片路徑
+CAM_RESULT_PATH = './data/'  #熱力圖的儲存位置
+
+if not os.path.exists(CAM_RESULT_PATH):
+    os.mkdir(CAM_RESULT_PATH)
+
 #加載圖像
-img_path = './dataset/images/CF PS DEFORMATION@NP@CF@CF_20220619_B5564PE-1-2.jpg'             #单张测试
 _, img_name = os.path.split(img_path)
-features_blobs = []
 img = Image.open(img_path).convert('RGB')
 img_tensor = (data_transform['val'](img)/255.0).unsqueeze(0).to(device) #[1,3,224,224]
 
+# 提取最后一層特徵圖
 features = model_features(img_tensor).detach()  #[1,2048,56,56]
-logit = model_ft(img_tensor)  #[1,2] -> [ 3.3207, -2.9495]
-
+# 獲得網路輸出
+logit = model_ft(img_tensor)
 h_x = torch.nn.functional.softmax(logit[2], dim=1).data.squeeze()  #tensor([0.9981, 0.0019])
-probs, idx = h_x.sort(0, True)      #按概率从大到小排列
-probs = probs.cpu().numpy()  #if tensor([0.0019,0.9981]) ->[0.9981, 0.0019]
-idx = idx.cpu().numpy()  #[1, 0]
-
+probs, idx = h_x.sort(0, True)     #按概率從大到小排列(信心度,索引值)
+probs = probs.cpu().numpy()
+idx = idx.cpu().numpy()
+# 獲得所有類別預測結果（含信心值,由大到小）
 for i in range(class_num):
     print('{:.3f} -> {}'.format(probs[i], class_[idx[i]]))  #打印预测结果
-
+# 獲得熱力圖的函數
 def returnCAM(feature_conv, weight_softmax, class_idx):
     bz, nc, h, w = feature_conv.shape        #1,2048,56,56
     output_cam = []
-    for idx in class_idx:  #只输出预测概率最大值结果不需要for循环
+    for idx in class_idx: 
         feature_conv = feature_conv.reshape((nc, h*w))  # [2048,56*56]
-        cam = torch.mm(weight_softmax[idx].unsqueeze(0),feature_conv)  #(1, 2048) * (2048, 56*56) -> (1, 56*56) （n,）是一个数组，既不是行向量也不是列向量
+        cam = torch.mm(weight_softmax[idx].unsqueeze(0),feature_conv)  #(1, 2048) * (2048, 56*56) -> (1, 56*56)
         cam = cam.reshape(h, w)
         cam_img = (cam - cam.min()) / (cam.max() - cam.min())  #Normalize
         cam_img = cam_img.detach().cpu().numpy()
         cam_img = np.uint8(255 * cam_img)                      #Format as CV_8UC1 (as applyColorMap required)
  
-        #output_cam.append(cv2.resize(cam_img, size_upsample))  # Resize as image size
         output_cam.append(cam_img)
     return output_cam
  
-# CAMs = returnCAM(features, fc_weights, [idx[0]])  #输出预测概率最大的特征图集对应的CAM
-CAMs = returnCAM(features, fc_weights, idx)  #输出预测概率最大的特征图集对应的CAM
+# CAMs = returnCAM(features, fc_weights, [idx[0]])  #得到預測信心度最高的類別對應的熱力圖
+CAMs = returnCAM(features, fc_weights, idx)  #得到所有預測類別的熱力圖
 print(img_name + ' output for the top1 prediction: %s' % class_[idx[0]])
-
+# 讀取圖片
 img = cv2.imread(img_path)
-height, width, _ = img.shape  #读取输入图片的尺寸
-heatmap = cv2.applyColorMap(cv2.resize(CAMs[0], (width, height)), cv2.COLORMAP_JET)  #CAM resize match input image size
-result = heatmap * 0.6 + img * 0.4    #比例可以自己调节
+height, width, _ = img.shape 
 
-text = '%s %.2f%%' % (class_[idx[0]], probs[0]*100) 				 #激活图结果上的文字显示
+heatmap = cv2.applyColorMap(cv2.resize(CAMs[0], (width, height)), cv2.COLORMAP_JET)  #CAM上顯示信心度最大值的class
+result = heatmap * 0.6 + img * 0.4    #比例可以自己調節
+
+text = '%s %.2f%%' % (class_[idx[0]], probs[0]*100)   #熱力圖上的文字
 cv2.putText(result, text, (20, 20), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5,
             color=(123, 222, 238), thickness=1, lineType=cv2.LINE_AA)
-CAM_RESULT_PATH = './dataset/'  #CAM结果的存储地址
+
+image_name_ = 'output'
+cv2.imwrite(CAM_RESULT_PATH + image_name_ + '_' + 'pred_' + class_[idx[0]] + '.jpg', result)  #写入存储磁盘
+""" 
+#----------------多張熱力圖繪圖-----------------
+imgpath = './dataset/detect_imgs/'  #detect圖片所在位置
+CAM_RESULT_PATH = './dataset/cam_pic/detect/cam_img/'   #熱力圖儲存資料夾路徑
+CAM_FALSE_PATH = './dataset/cam_pic/detect/cam_withorin_false/'  #分類錯誤的熱力圖與原圖儲存路徑
+CAM_RIGHT_PATH = './dataset/cam_pic/detect/cam_withorin_right/'  #分類正確的熱力圖與原圖儲存路徑
+
 if not os.path.exists(CAM_RESULT_PATH):
     os.mkdir(CAM_RESULT_PATH)
-image_name_ = 'output'
-cv2.imwrite(CAM_RESULT_PATH + image_name_ + '_' + 'pred_vit_' + class_[idx[0]] + '.jpg', result)  #写入存储磁盘
-""" 
-imgpath = './dataset/detect_imgs/'
+if not os.path.exists(CAM_FALSE_PATH):
+    os.mkdir(CAM_FALSE_PATH)
+if not os.path.exists(CAM_RIGHT_PATH):
+    os.mkdir(CAM_RIGHT_PATH)
 
+# 獲得熱力圖的函數
 def returnCAM(feature_conv, weight_softmax, class_idx):
     bz, nc, h, w = feature_conv.shape        #1,2048,56,56
     output_cam = []
-    for idx in class_idx:  #只输出预测概率最大值结果不需要for循环
+    for idx in class_idx:
         feature_conv = feature_conv.reshape((nc, h*w))  # [2048,56*56]
-        cam = torch.mm(weight_softmax[idx].unsqueeze(0),feature_conv)  #(1, 2048) * (2048, 56*56) -> (1, 56*56) （n,）是一个数组，既不是行向量也不是列向量
+        cam = torch.mm(weight_softmax[idx].unsqueeze(0),feature_conv)  #(1, 2048) * (2048, 56*56) -> (1, 56*56)
         cam = cam.reshape(h, w)
         cam_img = (cam - cam.min()) / (cam.max() - cam.min())  #Normalize
         cam_img = cam_img.detach().cpu().numpy()
         cam_img = np.uint8(255 * cam_img)                      #Format as CV_8UC1 (as applyColorMap required)
- 
-        #output_cam.append(cv2.resize(cam_img, size_upsample))  # Resize as image size
+
         output_cam.append(cam_img)
     return output_cam
-
-CAM_RESULT_PATH = './dataset/cam_pic/detect/cam_img/' 
 
 for j,img in enumerate(os.listdir(imgpath)):
     bg = Image.new('RGB',(448,224), "#000000")
     print(j)
     images = Image.open(imgpath+img).convert('RGB')
     img_tensor = (data_transform['val'](images)/255.0).unsqueeze(0).to(device) #[1,3,224,224]
+    # 提取最后一層特徵圖
     features = model_features(img_tensor).detach()  #[1,2048,56,56]
-
-    logit = model_ft(img_tensor)  #[1,2] -> [ 3.3207, -2.9495]
+    # 獲得網路輸出
+    logit = model_ft(img_tensor) 
     h_x = torch.nn.functional.softmax(logit[2], dim=1).data.squeeze()  #tensor([0.9981, 0.0019])
-    probs, idx = h_x.sort(0, True)      #按概率从大到小排列
-    probs = probs.cpu().numpy()  #if tensor([0.0019,0.9981]) ->[0.9981, 0.0019]
-    idx = idx.cpu().numpy()  #[1, 0]
+    probs, idx = h_x.sort(0, True)      #按概率從大到小排列(信心度,索引值)
+    probs = probs.cpu().numpy()
+    idx = idx.cpu().numpy()
         
-    CAMs = returnCAM(features, fc_weights, idx)  #输出预测概率最大的特征图集对应的CAM
+    CAMs = returnCAM(features, fc_weights, idx)  #得到所有預測類別的熱力圖
     print(img + ' output for the top1 prediction: %s' % class_[idx[0]])
-        
+    
+    # 讀取圖片    
     imgs = cv2.imread(imgpath+img)
-    height, width, _ = imgs.shape  #读取输入图片的尺寸
-    heatmap = cv2.applyColorMap(cv2.resize(CAMs[0], (height, width)), cv2.COLORMAP_JET)  #CAM resize match input image size
-    result = heatmap * 0.5 + imgs * 0.5    #比例可以自己调节
-    text = '%s %.2f%%' % (class_[idx[0]], probs[0]*100) 				 #激活图结果上的文字显示
+    img_name = img.split('@')
+    height, width, _ = imgs.shape
+    
+    # 獲取正確類別的索引值
+    t_key = 0
+    for i in range(14):
+        if img_name[0] == class_[idx[i]]:
+            t_key = i
+
+    #heatmap = cv2.applyColorMap(cv2.resize(CAMs[0], (width, height)), cv2.COLORMAP_JET)  #CAM上顯示信心度最大值的class
+    heatmap = cv2.applyColorMap(cv2.resize(CAMs[0], (height, width)), cv2.COLORMAP_JET)  #CAM上顯示True class
+    
+    result = heatmap * 0.5 + imgs * 0.5     #比例可以自己調節
+
+    #text = '%s %.2f%%' % (class_[idx[0]], probs[0]*100) 	#信心度最大值的class
+    text = '%s %.2f%%' % (class_[idx[0]], probs[0]*100) 	#True Class
     cv2.putText(result, text, (10, 20), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5,
             color=(123, 222, 238), thickness=1, lineType=cv2.LINE_AA)
-    img_name = img.split('@')
+
     cv2.imwrite(CAM_RESULT_PATH + img[:-4] + '_campred.jpg', result)
-    """ 
-    x = j%3
-    y = j//3
-    """ 
+    
+    #----------------熱力圖與原圖-----------------
+    bg = Image.new('RGB',(width*2,height), "#000000") #創建畫布(原圖與熱力圖)
     if img_name[0] != class_[idx[0]]:
         imgs = Image.open(imgpath+img)
         bg.paste(imgs,(0,0))
         im = Image.open(CAM_RESULT_PATH + img[:-4] + '_campred.jpg')
-        bg.paste(im,(224,0))
-        bg.save('./dataset/cam_pic/detect/cam_withorin_false/'+img[:-4]+'_cam.jpg')
+        bg.paste(im,(width,0))
+        bg.save(CAM_FALSE_PATH + img[:-4]+'_cam.jpg')
     else:
         imgs = Image.open(imgpath+img)
         bg.paste(imgs,(0,0))
         im = Image.open(CAM_RESULT_PATH + img[:-4] + '_campred.jpg')
-        bg.paste(im,(224,0))
-        bg.save('./dataset/cam_pic/detect/cam_withorin_right/'+img[:-4]+'_cam.jpg')
-  
-#bg.save('./dataset/cbam_pic/output.jpg')
-  
-exit()
+        bg.paste(im,(width,0))
+        bg.save(CAM_RIGHT_PATH + img[:-4]+'_cam.jpg')
